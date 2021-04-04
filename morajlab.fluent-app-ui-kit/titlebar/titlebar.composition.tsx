d05f3b9245a71dcd557d12f8f78d13a09c752b9f
morteza-jamali <mortezajamali4241@gmail.com>
2021-04-04 12:16:35 +0430

    fix: TitleBar farItems fixed

--- morajlab.fluent-app-ui-kit/titlebar/titlebar.composition.tsx
+++ morajlab.fluent-app-ui-kit/titlebar/titlebar.composition.tsx
@@ -1,70 +1,42 @@
-import React from 'react';
-import type { ICommandBarItemProps } from '@fluentui/react';
-import { Titlebar } from './titlebar';
-
-const _farItems: ICommandBarItemProps[] = [
-  {
-    key: 'tile',
-    text: 'Grid view',
-    // This needs an ariaLabel since it's icon-only
-    ariaLabel: 'Grid view',
-    iconOnly: true,
-    iconProps: { iconName: 'Tiles' },
-    onClick: () => console.log('Tiles'),
-  },
-  {
-    key: 'info',
-    text: 'Info',
-    // This needs an ariaLabel since it's icon-only
-    ariaLabel: 'Info',
-    iconOnly: true,
-    iconProps: { iconName: 'Info' },
-    onClick: () => console.log('Info'),
-  },
-];
+import React from "react";
+import type { ICommandBarItemProps } from "@fluentui/react";
+import { Titlebar } from "./titlebar";
 
 const _items: ICommandBarItemProps[] = [
   {
-    key: 'newItem',
-    text: 'New',
-    cacheKey: 'myCacheKey', // changing this key will invalidate this item's cache
-    iconProps: { iconName: 'Add' },
+    key: "newItem",
+    text: "New",
+    cacheKey: "myCacheKey", // changing this key will invalidate this item's cache
     subMenuProps: {
       items: [
         {
-          key: 'emailMessage',
-          text: 'Email message',
-          iconProps: { iconName: 'Mail' },
-          ['data-automation-id']: 'newEmailButton', // optional
+          key: "emailMessage",
+          text: "Email message",
+          iconProps: { iconName: "Mail" },
+          ["data-automation-id"]: "newEmailButton", // optional
         },
         {
-          key: 'calendarEvent',
-          text: 'Calendar event',
-          iconProps: { iconName: 'Calendar' },
+          key: "calendarEvent",
+          text: "Calendar event",
+          iconProps: { iconName: "Calendar" },
         },
       ],
     },
   },
   {
-    key: 'upload',
-    text: 'Upload',
-    iconProps: { iconName: 'Upload' },
-    href: 'https://developer.microsoft.com/en-us/fluentui',
+    key: "upload",
+    text: "Upload",
   },
   {
-    key: 'share',
-    text: 'Share',
-    iconProps: { iconName: 'Share' },
-    onClick: () => console.log('Share'),
+    key: "share",
+    text: "Share",
   },
   {
-    key: 'download',
-    text: 'Download',
-    iconProps: { iconName: 'Download' },
-    onClick: () => console.log('Download'),
+    key: "download",
+    text: "Download",
   },
 ];
 
 export const BasicTitlebar = () => {
-  return <Titlebar items={_items} farItems={_farItems} />;
+  return <Titlebar items={_items} />;
 };
--- morajlab.fluent-app-ui-kit/titlebar/titlebar.tsx
+++ morajlab.fluent-app-ui-kit/titlebar/titlebar.tsx
@@ -1,13 +1,108 @@
-import React from 'react';
-import { CommandBar, ICommandBarItemProps } from '@fluentui/react';
+import React from "react";
+import {
+  CommandBar,
+  IButtonStyles,
+  ICommandBarItemProps,
+  ICommandBarStyles,
+  IIconStyles,
+} from "@fluentui/react";
+
+const titleBarDefaultStyle: ICommandBarStyles = {
+  root: {
+    backgroundColor: "rgb(51, 51, 51)",
+    padding: 0,
+    "& button, & a": {
+      backgroundColor: "inherit",
+    },
+    "& i, & button, & a": {
+      color: "rgb(204, 204, 204)",
+    },
+  },
+};
+
+const getButtonStyle = (): IButtonStyles => {
+  let style: IButtonStyles = {
+    menuIcon: { display: "none" },
+  };
+
+  let sameRootProps: string[] = [
+    "rootHovered",
+    "rootFocused",
+    "rootExpandedHovered",
+    "rootPressed",
+  ];
+
+  sameRootProps.forEach((prop) => {
+    style[prop] = {
+      backgroundColor: "rgba(255,255,255,0.2)",
+      color: "rgb(204, 204, 204)",
+    };
+  });
+
+  return style;
+};
+
+const farItemsIconsDefaultStyle: IIconStyles = {
+  root: {
+    color: "rgb(204, 204, 204) !important",
+  },
+};
+
+const farItems: ICommandBarItemProps[] = [
+  {
+    key: "minimizeKey",
+    text: "Minimize",
+    ariaLabel: "Minimize",
+    iconOnly: true,
+    iconProps: {
+      iconName: "ChromeMinimize",
+      styles: farItemsIconsDefaultStyle,
+    },
+    buttonStyles: getButtonStyle(),
+  },
+  {
+    key: "maximizeKey",
+    text: "Maximize",
+    ariaLabel: "Maximize",
+    iconOnly: true,
+    iconProps: {
+      iconName: "ArrangeBringForward",
+      styles: farItemsIconsDefaultStyle,
+    },
+    buttonStyles: getButtonStyle(),
+  },
+  {
+    key: "closeKey",
+    text: "Close",
+    ariaLabel: "Close",
+    iconOnly: true,
+    iconProps: { iconName: "ChromeClose", styles: farItemsIconsDefaultStyle },
+    buttonStyles: {
+      rootHovered: {
+        backgroundColor: "red",
+      },
+    },
+  },
+];
 
 export interface ITitlebarProps extends React.HTMLAttributes<HTMLDivElement> {
   items: ICommandBarItemProps[];
-  farItems?: ICommandBarItemProps[];
 }
 
 export const Titlebar: React.FunctionComponent<ITitlebarProps> = ({
   items,
-  farItems,
   ...rest
-}) => <CommandBar items={items} farItems={farItems} {...rest} />;
+}) => {
+  items = items.map((item) =>
+    Object.assign(item, { buttonStyles: getButtonStyle() })
+  );
+
+  return (
+    <CommandBar
+      items={items}
+      farItems={farItems}
+      {...rest}
+      styles={titleBarDefaultStyle}
+    />
+  );
+};
